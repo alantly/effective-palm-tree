915c3f4e5fb6b7a5593d4b8da3b982817f7f7b2c
Alan <alanly211@gmail.com>
2017-06-16 22:59:32 -0700

    done with index

--- src/routes/ifttt/v1/index.spec.ts
+++ src/routes/ifttt/v1/index.spec.ts
@@ -1,10 +1,31 @@
 import * as Mocha from 'mocha';
 import { expect } from 'chai';
-import * as Supertest from 'supertest';
+import * as Express from 'express';
+import * as request from 'supertest';
+import routes from './index';
+
+const app = Express();
+app.use(routes);
 
 describe('Index routes', () => {
-  it('should return hello world', () => {
-    const result = 'Hello World!';
-    expect(result).to.equal('Hello World!');
+  describe('get /status', () => {
+    it('should return successfully', (done) => {
+      request(app)
+        .get('/status')
+        .expect(200, done);
+    });
+  });
+
+  describe('post /test/setup', () => {
+    it('should return a sample of the action and triggers', (done) => {
+      request(app)
+        .post('/test/setup')
+        .expect(200)
+        .then((resp) => {
+          expect(resp.body).has.nested.property('data.samples.triggers.new_dota_pro_game');
+          expect(resp.body).has.nested.property('data.samples.actions.send_sms_message');
+        })
+        .then(done);
+    });
   });
 });
